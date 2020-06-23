6e736a581b1d593229eba87d28d25295eb4f96d5
Pride Kavumba <pkavumba@gmail.com>
2020-06-24 01:24:11 +0900

    deploy

--- arxiv_vanity/settings.py
+++ arxiv_vanity/settings.py
@@ -102,7 +102,7 @@ DATABASES["default"].update(
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
-
+#
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
